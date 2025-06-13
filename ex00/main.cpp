620e7ee7f556b2d30c2ec3adc115565caa83e610
acta0724 <iwasakatsuya@icloud.com>
2025-06-14 03:48:30 +0900

    [fix] main.cpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,49 +1,21 @@
 #include <iostream>
 #include <string>
-#include "Whatever.hpp"
+#include "whatever.hpp"
 
-int main()
-{
-    int a = 2;
-    int b = 3;
-    std::cout << std::endl << "---int---" << std::endl;
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    ::swap(a, b);
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
-    std::cout << std::endl << "---float---" << std::endl;
-    float e = 2.1;
-    float f = 3.2;
-    std::cout << "e = " << e << ", f = " << f << std::endl;
-    ::swap(e, f);
-    std::cout << "e = " << e << ", f = " << f << std::endl;
-    std::cout << "min(e, f) = " << ::min(e, f) << std::endl;
-    std::cout << "max(e, f) = " << ::max(e, f) << std::endl;
-    std::cout << std::endl << "---char---" << std::endl;
-    char g = 'a';
-    char h = 'b';
-    std::cout << "g = " << g << ", h = " << h << std::endl;
-    ::swap(g, h);
-    std::cout << "g = " << g << ", h = " << h << std::endl;
-    std::cout << "min(g, h) = " << ::min(g, h) << std::endl;
-    std::cout << "max(g, h) = " << ::max(g, h) << std::endl;
-    std::cout << std::endl << "---double---" << std::endl;
-    double i = 2.1;
-    double j = 3.2;
-    std::cout << "i = " << i << ", j = " << j << std::endl;
-    ::swap(i, j);
-    std::cout << "i = " << i << ", j = " << j << std::endl;
-    std::cout << "min(i, j) = " << ::min(i, j) << std::endl;
-    std::cout << "max(i, j) = " << ::max(i, j) << std::endl;
-    std::cout << std::endl << "---string---" << std::endl;
-    std::string c = "chaine1";
-    std::string d = "chaine2";
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
-    std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
-    std::cout << std::endl;
-    return 0;
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
-    
