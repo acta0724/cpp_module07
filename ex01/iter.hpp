7799b26400fce92dd9660c650e70ff71631c7611
acta0724 <iwasakatsuya@icloud.com>
2025-06-14 04:31:05 +0900

    [fix] makefile

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -7,21 +7,15 @@
 template <typename T, typename F>
 void iter(T *array, std::size_t length, F f)
 {
-    for (std::size_t i = 0; i < length; i++)
+    for (std::size_t i = 0; i < length; ++i)
         f(array[i]);
 }
 
 template <typename T, typename F>
 void iter(const T *array, std::size_t length, F f)
 {
-    for (std::size_t i = 0; i < length; i++)
+    for (std::size_t i = 0; i < length; ++i)
         f(array[i]);
-}
-
-template <typename T>
-void print(T const &x)
-{
-    std::cout << x << std::endl;
 }
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 #include <cstddef>
 #include "iter.hpp"
+#include <string>
 
 void addOne(int &x) {
-    x++;
+    ++x;
+}
+
+template <typename T>
+void print(T const &x)
+{
+    std::cout << x << std::endl;
 }
 
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
     std::cout << "---Before---" << std::endl;
-    for (int i = 0; i < 5; i++) {
+    for (std::size_t i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
         std::cout << arr[i] << " ";
-    }
     std::cout << std::endl;
-    iter(arr, 5, addOne);
+    ::iter(arr, sizeof(arr) / sizeof(arr[0]), addOne);
     std::cout << std::endl << "---After---" << std::endl;
-    for (int i = 0; i < 5; i++) {
+    for (std::size_t i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
         std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
-    std::cout << std::endl << "---String---" << std::endl;
+    std::cout << std::endl << std::endl << "---String---" << std::endl;
     const std::string str[] = {"Hello", "World"};
-    iter(str, 2, print<std::string>);
+    ::iter(str, sizeof(str) / sizeof(str[0]), print<std::string>);
     return 0;
 }
